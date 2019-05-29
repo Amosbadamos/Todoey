240cd54f9e57c0f72d8a52872b865ff4dcf0666c
cosgrove <mattcos@gmail.com>
2019-05-29 00:54:30 -0400

    Implemented Core Data and it is working just like the video.

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UITableViewController {
     
     var itemArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //shared file manager object
         loadItems()
         
     }
@@ -49,6 +50,11 @@ class ToDoListViewController: UITableViewController {
         //This sets the value of the bool to the opposite of its current state.
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        // To delete an item when the row is selected.
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
+        
         saveItems()
             
         //this statement makes it so the cell doesn't not stay gray.
@@ -69,8 +75,11 @@ class ToDoListViewController: UITableViewController {
             //what will happen once the user clicks the Add Item button on our UIAlert
             //print(textField.text)
             
-            let newItem = Item()
+            //This creates an object in the staging area referred to as "context"
+            let newItem = Item(context: self.context)
+            //These set the attributes of the object
             newItem.title = textField.text!
+            newItem.done = false
             
             self.itemArray.append(newItem)
             
@@ -91,30 +100,56 @@ class ToDoListViewController: UITableViewController {
     
     func saveItems() {
         
-        let encoder = PropertyListEncoder()
+        //This commits the newly created object to the database.
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        
+        self.tableView.reloadData()
+        
+    }
+    // the optional paraeter gives you the opportunity to sort the tableview.reloaddata()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
         
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
-        } catch{
-            print("Error encoding item array, \(error)")
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
         }
         
         tableView.reloadData()
-        
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error decoding item array, \(error)")
+}
+    //MARK - Search bar methods
+
+extension ToDoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            
+            loadItems()
+            // This makes the search run on another thread in the background.
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+                
             }
+            
         }
     }
-    
 }
 
 
