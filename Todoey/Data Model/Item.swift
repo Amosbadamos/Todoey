fe98ea1e62c445b35381c4773a82c8490f8d42d4
cosgrove <mattcos@gmail.com>
2019-05-28 20:20:26 -0400

    Persisting data using Codable protocol and Plist.

--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -8,12 +8,7 @@
 
 import Foundation
 
-class Item {
+class Item: Encodable, Decodable {
     var title: String = ""
     var done: Bool = false
-    
-//    init(title: String, done: Bool){
-//        self.title = title
-//        self.done = done
-//    }
 }
